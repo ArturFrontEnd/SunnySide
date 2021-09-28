76eca44c46374e3c7fb2e6b528b08371b1f92cd8
FrontEndMentor-Artur <frontendmentorartur@gmail.com>
2021-09-28 17:46:20 -0300

    Finished Main-Clients, Main-Gallery is responsive

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import MainAbout from "./MainAbout";
 import MainHero from "./MainHero";
+import MainClients from "./MainClients";
+import MainGallery from "./MainGallery";
 
 const Main = (props) => {
   return (
     <main>
       <MainAbout />
       <MainHero />
+      <MainClients />
+      <MainGallery />
     </main>
   );
 };
