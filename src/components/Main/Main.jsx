cc9f312d1b61fab31c969f2a394c8dfe64ebba93
FrontEndMentor-Artur <frontendmentorartur@gmail.com>
2021-09-28 11:21:07 -0300

    Finished Main-Hero is responsive

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MainAbout from "./MainAbout";
+import MainHero from "./MainHero";
 
 const Main = (props) => {
   return (
     <main>
       <MainAbout />
+      <MainHero />
     </main>
   );
 };
